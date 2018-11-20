116440cecf1e1c05cb59d2cbaa7ba70acea594f5
Alexey Kochkarev <alexey.kochkarev@gmail.com>
2018-11-20 23:03:57 +0300

    Initial Commit

---
+++ opengl_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opengl_test
+//
+//  Created by Алексей Кочкарёв on 20.11.2018.
+//  Copyright © 2018 Алексей Кочкарёв. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
